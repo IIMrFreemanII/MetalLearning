e21e5a588b2e9183b2fb0a176704aa8892ec0316
ndiahovets <nikolay.diahovets@gmail.com>
2022-08-13 13:40:11 +0300

    Initial Commit

---
+++ MetalLearning/MetalLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalLearningApp.swift
+//  MetalLearning
+//
+//  Created by Nikolay Diahovets on 13.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct MetalLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
